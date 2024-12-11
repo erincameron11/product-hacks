18798a3f4591a931bdeab63ad1c2132cd17bf16d
erincameron11 <erincameron11@hotmail.com>
2024-12-11 12:48:55 -0500

    requirements bug fix

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 # ------------------------------------ IMPORTS ------------------------------------
 import streamlit as st
 import pandas as pd
-import streamlit_js_eval
 
 
 # ------------------------------------ MAIN APP ------------------------------------
 # Change the title and favicon of the app in the browser
-st.set_page_config(layout='wide', page_title='Product Hacks', page_icon=":bat:")
+st.set_page_config(layout='wide', page_title='Product Hacks', page_icon=":sparkles:")
 
-st.title('Product Hacks')
+st.title(':sparkles: Product Hacks')
 
 st.write(f'*Disclaimer: The products listed here are based on recommendations from TikTok creator [Nina Pool](https://www.tiktok.com/@ninaghoulina)*')
 
