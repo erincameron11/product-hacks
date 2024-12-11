8b9bdcfb0fff7f993a1d7e7997582ab1661a72b0
erincameron11 <erincameron11@hotmail.com>
2024-12-11 12:42:45 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+# ------------------------------------ IMPORTS ------------------------------------
+import streamlit as st
+import pandas as pd
+import streamlit_js_eval
+
+
+# ------------------------------------ MAIN APP ------------------------------------
+# Change the title and favicon of the app in the browser
+st.set_page_config(layout='wide', page_title='Product Hacks', page_icon=":bat:")
+
+st.title('Product Hacks')
+
+st.write(f'*Disclaimer: The products listed here are based on recommendations from TikTok creator [Nina Pool](https://www.tiktok.com/@ninaghoulina)*')
+
+st.divider()
+
+df = pd.read_csv('data.csv')
+df = df.drop('TikTok Reference Video', axis=1)
+
+st.dataframe(df, height=600, width=2000)
